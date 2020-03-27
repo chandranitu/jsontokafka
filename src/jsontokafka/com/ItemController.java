943e5505063da20fd92c51f210aa327725004bf0
hadoop <cnsnoida@gmail.com>
2020-03-28 05:19:28 +0530

    jasonToKafka

---
+++ src/jsontokafka/com/ItemController.java
@@ -0,0 +1,23 @@
+package jsontokafka.com;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("producer")
+public class ItemController {
+
+    @Autowired
+    KafkaTemplate<String, Item> KafkaJsontemplate;
+    String TOPIC_NAME = "sample";
+
+    @PostMapping(value = "/postItem",consumes = {"application/json"},produces = {"application/json"})
+    
+    public String postJsonMessage(@RequestBody Item item){
+        KafkaJsontemplate.send(TOPIC_NAME, new Item(1,"Lenovo","Laptop"));
+        return "Message published successfully";
+    }
+}
---
+++ src/jsontokafka/com/SpringKafkaJsonMsgApplication.java
@@ -0,0 +1,13 @@
+package jsontokafka.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaJsonMsgApplication {
+	
+	public static void main(String[] args) {
+	    SpringApplication.run(SpringKafkaJsonMsgApplication.class, args);
+	  }
+
+}
